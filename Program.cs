5c8d9c1b83b234d9f212fdb0dac7920730468598
giovaguty87 <gutygio87@gmail.com>
2019-05-31 15:07:05 -0500

    Initial load

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using Unicode;
+using System.IO;
+using System.Text;
+
+
+namespace ConsoleAppCheckEncoder
+{
+    class Program
+    {
+       //using (StreamReader sr = new StreamReader(txtName))
+       //{
+       //   sb.AppendLine(txtName.ToString());
+       //   sb.AppendLine("= = = = = =");
+       //   sb.Append(sr.ReadToEnd());
+       //   sb.AppendLine();
+       //   sb.AppendLine();
+       //}
+       //public static StringBuilder sb = new StringBuilder();
+
+        public static Utf8Checker Checker = new Utf8Checker();
+
+        //public static string fileName = "D:\\PersonalData\\BGlobal\\Translate\\Designer_es.json";
+        //public static string fileName = "D:\\PersonalData\\BGlobal\\Translate\\Login_es.json";
+        //public static string fileName = "D:\\PersonalData\\BGlobal\\Translate\\Registration_es.json";
+
+        //public static string fileName = "D:\\PersonalData\\BGlobal\\Translate\\WithOutEncode\\Designer_es.json";
+        //public static string fileName = "D:\\PersonalData\\BGlobal\\Translate\\WithOutEncode\\Login_es.json";
+        //public static string fileName = "D:\\PersonalData\\BGlobal\\Translate\\WithOutEncode\\Registration_es.json";
+        static void Main(string[] args)
+        {
+
+            foreach (string txtName in Directory.GetFiles(@"D:\\PersonalData\\BGlobal\\Translate", "*.json"))
+            {
+                var resp = Checker.Check(txtName.ToString());
+
+                Console.WriteLine($"File : {Path.GetFileName(txtName)} with encode UTF-8 : {resp}");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
